e2e14fd00129b6db76b4134ad20740373ae70a2a
aviviral <97469747+aviviral@users.noreply.github.com>
2022-04-30 17:00:36 +0530

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
     description1: "My name's Avinash Kumar. I'm a Full Stack Web Developer",
-    description2: "During the day I work as a lead developer at a local agency and in the evening I  work on freelance projects and utilize the time to built my own products. I spent my leisure hours writing articles and poetry. Right now I'm also trying a hand at machine learning and AI. I love to learn and explore new arenas.",
+    description2: "Innovative Web Developer with a passion to implement and launch new projects. Detail oriented professional with the ability to translate business requirements into technical solutions. Looking to start a career as an entry-level software engineer with a firm driven by technology.",
     image: 1
 }

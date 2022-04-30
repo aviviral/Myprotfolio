997085c7358b41a189f476daa1d40143cb9dde34
SHASHANK SHALABH <97499637+salove16@users.noreply.github.com>
2022-04-30 10:31:12 +0530

    Create headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Avinash kumar',
     title: "Full Stack Web Developer",
     desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
-    image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
+    image: 'https://avatars.githubusercontent.com/u/97469747?v=4',
     resumePdf: resume
 }

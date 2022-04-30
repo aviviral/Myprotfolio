b11741276bd1dcbf6c823966d78a01d62d811f09
aviviral <97469747+aviviral@users.noreply.github.com>
2022-04-30 16:01:12 +0530

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,7 +3,7 @@ import resume from '../assets/pdf/resume.pdf'
 export const headerData = {
     name: 'Avinash kumar',
     title: "Full Stack Web Developer",
-    desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
+    desciption:"Innovative Web Developer with a passion to implement and launch new projects. Detail oriented professional with the ability to translate business requirements into technical solutions. Looking to start a career as an entry-level software engineer with a firm driven by technology. ",
     image: 'https://avatars.githubusercontent.com/u/97469747?v=4',
     resumePdf: resume
 }

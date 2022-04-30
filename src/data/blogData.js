d8f213509694e625a23d1907cda8781ceb4ba689
aviviral <97469747+aviviral@users.noreply.github.com>
2022-04-30 15:43:53 +0530

    Update blogData.js

--- src/data/blogData.js
+++ src/data/blogData.js
@@ -1,19 +1,19 @@
 export const blogData = [
     {
         id: 1,
-        title: 'SCRAPING BILLBOARD AND IMDB USING PYTHON SELENIUM',
-        description: 'Here we will use Selenium to navigate between web pages, and try to scrape data from them.',
-        date: 'Aug 7, 2020',
-        image: 'https://1.bp.blogspot.com/-6vZQoWcIZeo/Xvw5ZD5Y53I/AAAAAAAABgI/GH2b5OuMaXAKuigK4diBbeYDW6dD3qVvgCK4BGAsYHg/w400-h225/selenium.jpg',
-        url: 'https://hackzism.blogspot.com/2020/07/scraping-billboard-and-imdb-using.html'
+        title: 'BeautyBebo Clone Website',
+        description: 'Here we will use HTML,CSS & Javascript to navigate between web pages, and try to scrape data from them.',
+        date: 'Dec 31, 2020',
+        image: 'https://miro.medium.com/max/700/1*dN6H7EwLpPHf-RFs4KAbrg.png',
+        url: 'https://medium.com/@aviviral/journey-of-creating-e-commerce-clone-in-our-construct-week-using-html-css-javascript-48732d98f995'
     },
     {
         id: 2,
-        title: 'CHECK WEATHER FROM TERMINAL USING WTTR.IN',
-        description: 'wttr.in is a console-oriented weather forecast service that supports various information representation methods like ANSI-sequences for console HTTP clients, HTML, or PNG.',
-        date: 'Aug 11, 2020',
-        image: 'https://1.bp.blogspot.com/-OW7jX57tea4/XvnGxuEOslI/AAAAAAAABW0/R8lVT1AXDSwnvE0EGA9Ra49-LDm1ACwDgCK4BGAsYHg/s1216/wttr1.png',
-        url: 'https://hackzism.blogspot.com/2020/06/check-weather-from-terminal-using-wttrin.html'
+        title: 'Pepperfry.com Clone',
+        description: 'Here we will use HTML,CSS,Node.js & Javascript to navigate between web pages, and try to scrape data from them.',
+        date: 'Feb 4, 2021',
+        image: 'https://miro.medium.com/max/700/0*LZi3E1R-YhEdpLfe.jpeg',
+        url: 'https://medium.com/@aviviral/the-cloned-website-cloning-period-of-pepperfry-com-2682876d04f0'
     },
 //     {
 //         id: 3,

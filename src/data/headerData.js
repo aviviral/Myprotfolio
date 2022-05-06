53fd3c9fcb6089013f3fd7b0dbb7f97809796468
Avinash Kumar <97469747+aviviral@users.noreply.github.com>
2022-05-06 11:03:30 +0530

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,9 @@
-import resume from '../assets/pdf/resume.pdf'
+// import resume from '../assets/pdf/resume.pdf'
 
 export const headerData = {
     name: 'Avinash kumar',
     title: "Full Stack Web Developer",
     desciption:"Innovative Web Developer with a passion to implement and launch new projects. Detail oriented professional with the ability to translate business requirements into technical solutions. Looking to start a career as an entry-level software engineer with a firm driven by technology. ",
     image: 'https://avatars.githubusercontent.com/u/97469747?v=4',
-    resumePdf: resume
+    resumePdf: "https://drive.google.com/file/d/1UzKrZNKAIVZ3g7RAiSefNYLzcdoeqfzy/view?usp=sharing"
 }
